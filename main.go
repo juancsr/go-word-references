aa6c2bc164a3a4144713d519d84a50499c814b9a
juancsr <juancsr@protonmail.com>
2021-04-01 21:54:11 -0500

    Container environment updated
    - Air added

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
- //   db "github.com/juancsr/go-word-references/db"
-    router "github.com/juancsr/go-word-references/router"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    print("running main function \n")
-    router.Run()
+	fmt.Println("running app...")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ponga",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
-
-
--- models/word.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type Words struct {
-    ID          primitive.ObjectID  `json:"_id" bson:"_id,omitempty"`
-    Word        string              `json:"word" bson:"word"`
-    Language    string              `json:"language" bson:"language"`
-}
